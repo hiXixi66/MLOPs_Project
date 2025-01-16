c214026f61f628db2f8791a322892d39fbc82035
Viktor <isaksenviktor@gmail.com>
2025-01-16 15:29:57 +0100

    finalized hydra setup. It saves stuff in output/ not models/ and reports/

--- src/rice_images/train.py
+++ src/rice_images/train.py
@@ -1,31 +1,38 @@
+import os
 import matplotlib.pyplot as plt
 import torch
-import typer
 from rice_images.data import load_data
 from rice_images.model import load_resnet18
-import os
+from omegaconf import DictConfig, OmegaConf
+import hydra
 
 # Set the device
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
-
-def train(
-    lr: float = 1e-3,
-    batch_size: int = 32,
-    epochs: int = 2,
-    epoch_save_interval: int = 1,
-    model_save_path: str = "tester",
-    downsample_train: int = 10
-):
+@hydra.main(config_path="../../configs", config_name="train", version_base="1.1")
+def train(cfg: DictConfig):
     """Train the ResNet-18 model on the rice images dataset and save parameters every epoch_save_interval epochs."""
-    print("Training day and night")
-    print(f"{lr=}, {batch_size=}, {epochs=}, {epoch_save_interval=}, {model_save_path=}")
+    print(OmegaConf.to_yaml(cfg))
 
-    # Create the folder to save model parameters if it doesn't exist
+    # Define the working directories of the original script and Hydra's new one
+    hydra_wd = os.getcwd()
+    original_wd = hydra.utils.get_original_cwd()
+
+    # Explicitly define hyperparameters
+    lr = cfg.lr
+    batch_size = cfg.batch_size
+    epochs = cfg.epochs
+    epoch_save_interval = cfg.epoch_save_interval
+    model_save_path = cfg.model_save_path
+    downsample_train = cfg.downsample_train
+
+    # Create the folder to save model parameters if it doesn't exist in hydra: outputs/_date_/
     os.makedirs(f"models/{model_save_path}", exist_ok=True)
 
     # Load the data
+    os.chdir(original_wd) # change to original directory to load data
     train_dataset, val_dataset, test_dataset = load_data()
+
     # Downsample train_dataset to make the code run faster
     num_samples_train = len(train_dataset) // downsample_train
     downsampled_indices = torch.randperm(len(train_dataset))[:num_samples_train]
@@ -74,12 +81,17 @@ def train(
         print(f"Epoch {epoch} completed. Loss: {epoch_loss}, Accuracy: {accuracy * 100:.2f}%")
 
         # Save model parameters every epoch_save_interval epochs
-        if (epoch + 1) % epoch_save_interval == 0:  # Save after every 5 epochs (or given interval)
+        if (epoch + 1) % epoch_save_interval == 0:
+            os.chdir(hydra_wd) # switch back to hydra working directory
+
             checkpoint_path = f"models/{model_save_path}/resnet18_epoch_{epoch+1}.pth"
             torch.save(model.state_dict(), checkpoint_path)
             print(f"Model parameters saved at {checkpoint_path}")
 
+            os.chdir(original_wd) # change to original directory to load data
+
     # Save the final trained model
+    os.chdir(hydra_wd) # switch back to hydra working directory
     torch.save(model.state_dict(), f"models/{model_save_path}/resnet18_rice_final.pth")
 
     # Plot statistics
@@ -90,10 +102,11 @@ def train(
     axs[1].plot(statistics["train_accuracy"], label="Train Accuracy")
     axs[1].set_title("Train Accuracy")
     axs[1].legend()
+    # Save figure
+    os.makedirs(f"reports/figures", exist_ok=True)
     fig.savefig("reports/figures/training_statistics.png")
     print("Training complete and statistics saved.")
 
 
-
 if __name__ == "__main__":
-    typer.run(train)
+    train()
